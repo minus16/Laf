754682b90781f5e2e9f9611ac45c1419e4017f86
minus16 <minus16@gmail.com>
2017-04-14 07:38:44 +0900

    1

--- src/test1/test1.java
+++ src/test1/test1.java
@@ -1,5 +1,30 @@
 package test1;
 
 public class test1 {
-
+	
+	//test cases
+	static final char[] testcase1 = {};
+	static final char[] testcase2 = {1};
+	static final char[] testcase3 = {1,3};
+	static final char[] testcase4 = {1,2,3};
+	static final char[] testcase5 = {1,2,3,6,8,9,10};
+	static final char[] testcase6 = {13,14,15,16,20,23,24,25,100};
+	
+	
+	public test1()
+	{
+		Readnumbers(testcase1);
+		Readnumbers(testcase2);
+		Readnumbers(testcase3);
+		Readnumbers(testcase4);
+		Readnumbers(testcase5);
+		Readnumbers(testcase6);
+		
+	}
+	
+	public void Readnumbers(char[] data)
+	{
+		
+	}
+	
 }
